e736b3334ff64d379a2e2e84b90a0ba5b4582b2f
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-11 19:21:27 -0300

    feat: create Guest class

---
+++ Models/Guest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace console_hotel_system.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string IdDocument { get; set; }
+
+        public Guest(
+            int id,
+            string fullName,
+            int age,
+            string birthday,
+            string gender,
+            string address,
+            string phone,
+            string email,
+            string idDocument
+            )
+        {
+            Id = id;
+            FullName = fullName;
+            Age = age;
+            Birthday = birthday;
+            Gender = gender;
+            Address = address;
+            Phone = phone;
+            Email = email;
+            IdDocument = idDocument;
+        }
+    }
+}
