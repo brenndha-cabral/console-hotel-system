d11c53aed1ce528d55dc30b61d1b3c45ec139b57
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-11 19:21:54 -0300

    feat: create Suite class

---
+++ Models/Suite.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace console_hotel_system.Models
+{
+    public class Suite
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+        public string Details { get; set; }
+
+        public Suite
+        (
+            int id,
+            string type,
+            int capacity,
+            decimal price,
+            string details
+        )
+        {
+            Id = id;
+            Type = type;
+            Capacity = capacity;
+            Price = price;
+            Details = details;
+        }
+    }
+}
